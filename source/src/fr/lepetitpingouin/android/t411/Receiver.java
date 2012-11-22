ab67fe96c7bb990b64b4d8ca05956aee37b70e5a
meyergre <meyergre@gmail.com>
2012-11-22 23:29:29 +0100

    0.9.1-out-of-market
    testing keyguard clock-widget
    added tablet-layout for main application view

--- source/src/fr/lepetitpingouin/android/t411/Receiver.java
+++ source/src/fr/lepetitpingouin/android/t411/Receiver.java
@@ -1,5 +1,7 @@
 package fr.lepetitpingouin.android.t411;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -15,5 +17,11 @@ public class Receiver extends BroadcastReceiver {
 
 		Intent intent = new Intent(ctx, t411updater.class);
 		ctx.startService(intent);
+		
+		Intent intent2 = new Intent("android.intent.ACTION_TIME_CHANGED");
+		PendingIntent pi = PendingIntent.getBroadcast(ctx, 0, intent2, 0);
+		AlarmManager am = (AlarmManager) ctx.getSystemService(ctx.ALARM_SERVICE);
+		am.cancel(pi); // cancel any existing alarms
+		am.setRepeating(AlarmManager.RTC, 2000, 2000, pi);
 	}
 }
